91861d3271f3dcb22546805d6036ecdae89545cc
codepnw <idchokms01@outlook.com>
2024-03-31 21:25:32 +0700

    completed token bucket

---
+++ token-bucket/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Message struct {
+	Status string `json:"status"`
+	Body   string `json:"body"`
+}
+
+func endpointHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	message := Message{
+		Status: "Successful",
+		Body:   "Hi!, You are reached the API. How may i help you?",
+	}
+	err := json.NewEncoder(w).Encode(&message)
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+	http.Handle("/ping", rateLimiter(endpointHandler))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println("There was an error listening on port :8080", err)
+	}
+}
